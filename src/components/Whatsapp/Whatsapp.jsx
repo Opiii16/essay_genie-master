3896fc7120a38e0cd6d098bd42419bd961cb973e
Opiii16 <oprahjane860@gmail.com>
2025-05-22 13:39:52 +0300

    wa numbercommit

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 import './Whatsapp.css';
 
 const WhatsAppButton = () => {
-  const whatsappNumber = '+254791042547';
+  const whatsappNumber = '+254717778496';
   const message = 'Hello EssayGenie, I need help with my essay! Can you help please?';
   const preFilledMessage = `Hello! Thank you for contacting EssayGenie.com - the best assignment help platform for all your high school and college needs. Please hold on and save our contact while our support team connects with you. We typically respond within 5 minutes during business hours (9AM-5PM).`;
 
